1a450b33365ceae0d863aa6bec65174367af1ab1
piekarskipiotr <ppiekarski8@gmail.com>
2021-12-15 20:21:25 +0100

    Add localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:payflix/resources/app_theme.dart';
+import 'package:payflix/resources/l10n/l10n.dart';
 import 'package:payflix/resources/routes/app_routes.dart';
 import 'package:payflix/resources/routes/routes_handler.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,6 +22,15 @@ class MyApp extends StatelessWidget {
       // routes
       initialRoute: AppRoutes.playground,
       onGenerateRoute: RoutesHandler().getRoute,
+
+      // localization
+      supportedLocales: L10n.all,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
 
       // theme
       themeMode: ThemeMode.light,
